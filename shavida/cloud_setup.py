3b96d8af2e32e0e28b090da95edc65dab4f15e81
Kom Sihon <rsihon@gmail.com>
2019-08-26 14:17:06 +0100

    Re-sync with production platform

--- shavida/cloud_setup.py
+++ shavida/cloud_setup.py
@@ -25,7 +25,7 @@ from ikwen.accesscontrol.backends import UMBRELLA
 from ikwen.accesscontrol.models import SUDO, Member
 from ikwen.billing.models import Invoice, PaymentMean, InvoicingConfig, JUMBOPAY_MOMO
 from ikwen.billing.utils import get_next_invoice_number
-from ikwen.conf.settings import STATIC_ROOT, STATIC_URL, MEDIA_ROOT, MEDIA_URL
+from ikwen.conf.settings import STATIC_ROOT, STATIC_URL, CLUSTER_MEDIA_ROOT, CLUSTER_MEDIA_URL
 from ikwen.core.models import Service, OperatorWallet, SERVICE_DEPLOYED
 from ikwen.core.tools import generate_django_secret_key, generate_random_key, reload_server
 from ikwen.core.utils import add_database_to_settings, add_event, get_mail_content, \
@@ -109,8 +109,8 @@ def deploy(app, member, business_type, project_name, billing_plan, theme, monthl
     # Create a copy of template application in the Cloud folder
     app_folder = CLOUD_HOME + '000Tpl/AppSkeleton'
     website_home_folder = CLOUD_FOLDER + ikwen_name
-    media_root = MEDIA_ROOT + ikwen_name + '/'
-    media_url = MEDIA_URL + ikwen_name + '/'
+    media_root = CLUSTER_MEDIA_ROOT + ikwen_name + '/'
+    media_url = CLUSTER_MEDIA_URL + ikwen_name + '/'
     default_images_folder = CLOUD_FOLDER + '000Tpl/images/000Default'
     if os.path.exists(default_images_folder):
         if os.path.exists(media_root):
